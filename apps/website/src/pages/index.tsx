41fe24787d93cdba811dc23962935a77d551d75d
Shadow <shadow@buape.com>
2023-09-16 12:55:13 -0500

    home stuff

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from "clsx"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
-import HomepageFeatures from "@site/src/components/HomepageFeatures"
 
 import styles from "./index.module.css"
 
@@ -27,11 +26,11 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext()
     return (
-        <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+        <Layout title={`CrossBuild`} description="Docs for CrossBuild">
             <HomepageHeader />
-            <main>
+            {/* <main>
                 <HomepageFeatures />
-            </main>
+            </main> */}
         </Layout>
     )
 }
