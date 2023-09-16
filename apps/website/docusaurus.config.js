62cedab68c09fe9770fcf6ee17717e58c9fae60a
Shadow <shadow@buape.com>
2023-09-16 12:37:11 -0500

    deployment setup

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
+    trailingSlash: false,
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
